8c45b2414e999649097b58d5a92000da08d985e8
Kaviputhalvan <89631793+Kaviputhalvan@users.noreply.github.com>
2025-03-07 09:43:44 +0530

    Create kernel-root-module.c

---
+++ source_build/kernel-root-module.c
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/fb.h>
+#include <linux/miscdevice.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("KAVIPUTHALVAN");
+MODULE_DESCRIPTION("");
+MODULE_VERSION("0");
+
